3abde4870f42da3f4022b1cec52e7c6b1c0c0e58
Joao-Victor-Oliveira <106411373+Joao-Victor-Oliveira@users.noreply.github.com>
2022-11-09 22:22:21 -0300

    Add files via upload

--- include/Ente/Menus/Menu.h
+++ include/Ente/Menus/Menu.h
@@ -1,16 +1,41 @@
 #ifndef MENU_H
 #define MENU_H
-
 #include "../../Ente/Ente.h"
 
-class Menu : public Ente
+using namespace Gerenciadores;
+
+class Menu:protected Ente
 {
-    public:
-
-
-    protected:
-
     private:
+    sf::Text text[3];
+    sf::Text fases[3];
+    sf::Font font;
+    sf::RenderWindow* window;
+    Gerenciador_Grafico* grafico;
+    class Contador{
+    private:
+        int numero;
+        int Max;
+    public:
+        Contador(int Max){numero=0; this->Max= Max-1;};
+        ~Contador(){};
+        void operator++(int){
+            numero++;
+            if(numero>Max)
+                numero=0;};
+        void operator--(int){
+            numero--;
+            if(numero<0)
+                numero=Max;};
+        int getContador(){return numero;}
+    };
+    Contador n;
+    public:
+    Menu(Gerenciador_Grafico* gerenciador=NULL);
+    ~Menu();
+    void executar();
+    void MostraFases();
+    void imprimir_se(int tela);
 };
 
 #endif // MENU_H
